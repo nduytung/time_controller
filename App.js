aadc63564d77c39762450ae4c095527c9006a6ee
AnhQuach66 <anhhquachh@gmail.com>
2021-11-05 20:10:48 +0700

    RankScreen

--- App.js
+++ App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { NavigationContainer } from "@react-navigation/native";
 import Login from './screens/Login';
 import RootStackScreen from "./screens/RootStack";
+import RankScreen from './screens/RankScreen';
+
+
 const App = () => {
   //dua screen can hien thi vao day
   return (
     <NavigationContainer>
       <RootStackScreen/>
     </NavigationContainer>
+    //<RankScreen></RankScreen>
   )
 };
 
